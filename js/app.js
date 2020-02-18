795c9503d5cb0a5091e7c965c2830080ff23d9de
qasembanyissa <q0790405801@gmail.com>
2020-02-18 17:02:58 +0200

    lab2 added

--- js/app.js
+++ js/app.js
@@ -1,53 +1,121 @@
 'use strict';
 
 var xz = prompt('what is your name');
-
+var counter = 0;
 
 var n1 = prompt('my name is qasem ? yes/no ');
 if (n1.toLowerCase() === 'yes' || n1.toLowerCase() === 'y') {
-    alert('correct');
+  alert('correct');
+  counter ++;
 } else if (n1.toLowerCase() === 'no' || n1.toLowerCase() === 'n') {
-    alert('incorrect , my name is qasem');
+  alert('incorrect , my name is qasem');
 } else {
-    alert('try again');
+  alert('try again');
 }
 var n2 = prompt('i have 30 years ? yes/no ');
 if (n2.toLowerCase() === 'yes' || n2.toLowerCase() === 'y') {
-    alert('incorrect! , i have 24 years');
+  alert('incorrect! , i have 24 years');
 } else if (n2.toLowerCase() === 'no' || n2.toLowerCase() === 'n') {
-    alert('corrct , i have 24 years');
+  alert('corrct , i have 24 years');
+  counter ++;
 } else {
-    alert('try again');
+  alert('try again');
 }
 
 var n3 = prompt('my favourt hoppy is reading? yes/no');
 if (n3.toLowerCase() === 'yes' || n3.toLowerCase() === 'y') {
-    alert('incorrect , i dont like reading , i like sport');
+  alert('incorrect , i dont like reading , i like sport');
 } else if (n3.toLowerCase() === 'no' || n3.toLowerCase() === 'n') {
-    alert('correct , i like sport ');
+  alert('correct , i like sport ');
+  counter ++;
 } else {
-    alert('try again');
+  alert('try again');
 } var n4 = prompt('my favourt club is real madrid ? yes/no');
 if (n4.toLowerCase() === 'yes' || n4.toLowerCase() === 'y') {
-    alert('correct');
+  alert('correct');
+  counter ++;
 } else if (n4.toLowerCase() === 'no' || n4.toLowerCase() === 'n') {
-    alert('incorrect! , my favourt club is real madrid');
+  alert('incorrect! , my favourt club is real madrid');
 } else {
-    alert('try again');
+  alert('try again');
 }
 
 var n5 = prompt('iam love with coffee ? yes/no');
 if (n5.toLowerCase() === 'yes' || n5.toLowerCase() === 'y') {
-    alert('correct');
+  alert('correct');
+  counter ++;
 } else if (n5.toLowerCase() === 'no' || n5.toLowerCase() === 'n') {
-    alert('incorrect , i love coffe of course');
+  alert('incorrect , i love coffe of course');
 } else {
-    alert('try again');
+  alert('try again');
 }
 
 alert('welcome  ' + xz);
-document.write ('welcome  ' + xz);
+document.write('welcome  ' + xz);
+// var i = 0;
+// for (i=0;i<4;i++)
+//   var qw=prompt('i need ypu to choise anumber between 1-10 , and i need you to guess what is number , you have four chanses to correct a wright answer');
 
+var num = 0;
+
+var q6 = prompt('i need ypu to choise anumber i added it , and i need you to guess what is number , you have four chanses to correct a wright answer');
+
+while (num < 4) {
+  if (q6 == 15) {
+    alert('great , good job , this is the right number');
+    counter ++;
+    break;
+  }
+  else if (q6 < 10) {
+    alert('The number is small, choose a higher number');
+    q6 = prompt('try again');
+    num++;
+    console.log('first try',num)
+  }
+  else if (9 < q6 && q6< 15) {
+    alert('Choose a slightly higher number, you got close to answering!');
+    q6 = prompt('try again');
+    console.log('second try',num);
+    num++;
+
+  }
+
+  else if (q6>15 && q6<21) {
+    alert('Choose a slightly lower number, you came close to answering!');
+    q6 = prompt('i need ypu to choise anumber i added it , and i need you to guess what is number , you have four chanses to correct a wright answer');
+    console.log('third try',num);
+    num++;
+  }
+  else if ( q6 > 22 ) {
+    alert('The number is high, choose a lower number');
+    q6 = prompt('i need ypu to choise anumber i added it , and i need you to guess what is number , you have four chanses to correct a wright answer');
+    console.log('fourth try',num)
+    num++;
+
+  } else {
+    alert('wrong answer , please choise a number');
+    q6 = prompt('i need ypu to choise anumber i added it , and i need you to guess what is number , you have four chanses to correct a wright answer');
+    num++;
+  }
+}
+
+var films=['intersteller','inception','django','LOTR','The VOW'];
+var z=0;
+for(z;z<films.length;z++){
+  var b =prompt('whate is my favourt film ?');
+  if(b === films[z]){
+    alert('correct answer');
+    counter ++;
+    break;
+  }else{
+    alert('wrong answer');
+  }
+
+}
+if (z === films.length){
+  alert('you are used all attemps '+films);
+}
+document.write('<p id = doc> '+xz+', thanke you , your good '+counter+'<P>');
 
 
 
